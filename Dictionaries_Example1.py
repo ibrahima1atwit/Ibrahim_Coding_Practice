1e6bea9fd9ebe7ba6579721cb8882124fbb136ee
ibrahima1atwit <123700317+ibrahima1atwit@users.noreply.github.com>
2023-09-02 23:23:29 -0400

    Create Dictionaries_Example1.py

---
+++ Dictionaries_Example1.py
@@ -0,0 +1,11 @@
+#Key: State
+#Value: Capital
+
+statesToCaptal = {}
+
+//Adding a few key-value pairs
+statesToCapital["Texas"] =  "Austin"
+statesToCapital["Massachusetts"] =  "Boston"
+
+print(statesToCapital["New York"]) // gets access to values that have a key matching "New York"
+
