dd089335a4f3924823a1f00791fb69c39a4a248b
ibrahima1atwit <123700317+ibrahima1atwit@users.noreply.github.com>
2023-09-02 21:42:20 -0400

    Update Linked_List_Example2.java

--- Linked_List_Example2.java
+++ Linked_List_Example2.java
@@ -5,11 +5,26 @@ public class MyClass {
     LinkedList travelBucketList = new LinkedList();
 
     // Add Items
-    travelBucketList.add("Mogadishu, Somalia");
+    travelBucketList.add("Mogadishu, Somalia");  
     travelBucketList.add("Barcelona, Spain");
     travelBucketList.add("Santorini, Greece");
     travelBucketList.add("Toronto, Canada");
     travelBucketList.add(2, "Los Angeles, California");
     System.out.println(travelBucketList);
+
+    // Access
+    System.out.println(travelBucketList.get(2));
+    System.out.println(travelBucketList.getFirst());
+
+    System.out.println(travelBucketList.contain("Barcelona, Spain"));
+
+    // Remove Items
+    travelBucketList.removeFirst();
+    travelBucketList.removeLast();
+    System.out.println(travelBucketList);
+
+    travelBucketList.remove("Santorini, Greece");  // remove by object
+    travelBucketList.remove(0); // remove by index
+    System.out.println(travelBucketList);  // print contents of LL
   }
 }
