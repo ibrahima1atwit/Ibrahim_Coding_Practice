4d6856fbd5ddacf6a4ff188dc23679d294fa7e4a
ibrahima1atwit <123700317+ibrahima1atwit@users.noreply.github.com>
2023-09-02 22:30:51 -0400

    Create Stack_Example2.java

---
+++ Stack_Example2.java
@@ -0,0 +1,8 @@
+import java.util.Stack
+public class Myclass {
+  public static void main(String arg[]) {
+    Stack myStack = new Stack();             // Creating a new stack Data Structure called 'myStack'
+    myStack.push("hi");                      // Pushed a string phrase called 'Hi" onto the top of the stack
+    myStack.pop();                           // Pushed that same string phase from the top of the stack
+  }
+}
